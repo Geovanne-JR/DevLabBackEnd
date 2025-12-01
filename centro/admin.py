38c0230139571faeead1877141381f17bd92d5f1
Geovanne <geovannejunio113@gmail.com>
2025-12-01 00:25:10 -0300

    registrando modelos no admin

--- centro/admin.py
+++ centro/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Usuario
+from .models import Projeto
+from .models import Equipes
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Projeto)
+admin.site.register(Equipes)
+
